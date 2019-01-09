0015bb2ce80432dc405e7c07da437e60a8cfb4ab
HBKarim <find_yourlove2@yahoo.com>
2019-01-09 10:49:11 +0200

    Updates

--- precache-manifest.50499ed89d5e1c3a11e1a17a57c1797d.js
+++ precache-manifest.50499ed89d5e1c3a11e1a17a57c1797d.js
@@ -4,24 +4,24 @@ self.__precacheManifest = [
     "url": "/fullwebsite-Royate/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "0b224c5d5407014f549d",
-    "url": "/fullwebsite-Royate/static/css/main.9b8e6bc3.chunk.css"
+    "revision": "16b939abc7ac4184c0c0",
+    "url": "/fullwebsite-Royate/static/css/main.ed001f7e.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/fullwebsite-Royate/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "17b217a6eedeba306a70",
-    "url": "/fullwebsite-Royate/static/js/1.17b217a6.chunk.js"
+    "revision": "b54ffe44a5e3e3a3edaa",
+    "url": "/fullwebsite-Royate/static/js/1.b54ffe44.chunk.js"
   },
   {
     "revision": "734f0538b22106bd7539",
     "url": "/fullwebsite-Royate/static/js/runtime~main.734f0538.js"
   },
   {
-    "revision": "0b224c5d5407014f549d",
-    "url": "/fullwebsite-Royate/static/js/main.0b224c5d.chunk.js"
+    "revision": "16b939abc7ac4184c0c0",
+    "url": "/fullwebsite-Royate/static/js/main.16b939ab.chunk.js"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "/fullwebsite-Royate/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "17b217a6eedeba306a70",
-    "url": "/fullwebsite-Royate/static/css/1.d7b1e530.chunk.css"
+    "revision": "b54ffe44a5e3e3a3edaa",
+    "url": "/fullwebsite-Royate/static/css/1.7212f306.chunk.css"
   },
   {
-    "revision": "9d64af5c2db11cc5bb06dab00094e2ca",
+    "revision": "a427cf229804d63f632fb4b5b7e77cb1",
     "url": "/fullwebsite-Royate/index.html"
   }
 ];
