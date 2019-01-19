10df40d302726145ae94522f53dffec138054ab0
HBKarim <find_yourlove2@yahoo.com>
2019-01-19 10:43:53 +0200

    Updates

---
+++ precache-manifest.4d9fdea1d4f2fedbeafac45e5a50210f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fullwebsite-Royate/home/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ed5da5e450d7fa86385",
+    "url": "/fullwebsite-Royate/home/static/css/main.a12780f7.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fullwebsite-Royate/home/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9f8349442334004d18a7",
+    "url": "/fullwebsite-Royate/home/static/js/1.9f834944.chunk.js"
+  },
+  {
+    "revision": "46c8572ba8140c661b2d",
+    "url": "/fullwebsite-Royate/home/static/js/runtime~main.46c8572b.js"
+  },
+  {
+    "revision": "4ed5da5e450d7fa86385",
+    "url": "/fullwebsite-Royate/home/static/js/main.4ed5da5e.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fullwebsite-Royate/home/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fullwebsite-Royate/home/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fullwebsite-Royate/home/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "9f8349442334004d18a7",
+    "url": "/fullwebsite-Royate/home/static/css/1.bd62bff2.chunk.css"
+  },
+  {
+    "revision": "11402e9458194a76e69dfdb3cc974c71",
+    "url": "/fullwebsite-Royate/home/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fullwebsite-Royate/precache-manifest.38ca02e8b9f7e00359a5a50ce0891691.js"
+  "/fullwebsite-Royate/home/precache-manifest.4d9fdea1d4f2fedbeafac45e5a50210f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fullwebsite-Royate/index.html", {
+workbox.routing.registerNavigationRoute("/fullwebsite-Royate/home/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
